8c445a44c01ec31a31a8646253c2566eec69a48f
Rui Neves Carvalho <astralenigma@hotmail.com>
2016-11-21 19:01:18 +0000

    Adicionadas permissões para localização e algumas variáveis ao alarme. Fuck this shit

--- app/src/main/java/procrastinadoresprofissionais/zonelembrete/Alarme.java
+++ app/src/main/java/procrastinadoresprofissionais/zonelembrete/Alarme.java
@@ -1,8 +1,14 @@
 package procrastinadoresprofissionais.zonelembrete;
 
+import java.util.Date;
+
 /**
  * Created by Rui on 18-Nov-16.
  */
 
-class Alarme {
+public class Alarme {
+    String Nome;
+    Date dataInicio;
+    Date dataFim;
+
 }
